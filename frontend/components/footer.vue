0a0e77e3790fc8995a16e7c1c7a5b549df7b3723
Gabriel Landry <57534192+Sportek@users.noreply.github.com>
2024-01-20 15:44:40 -0500

    Ajout

---
+++ frontend/components/footer.vue
@@ -0,0 +1,32 @@
+<template>
+  <footer>
+    <div class="about-us">
+      <div class="frontend">
+        <div class="team">
+          <div>Gabriel Landry</div>
+          <div>Luckas Rakotondrafara</div>
+        </div>
+      </div>
+      <div class="backend">
+        <div class="team">
+          <div>Marc-Antoine Manningham</div>
+          <div>Gaëtan Florio</div>
+        </div>
+      </div>
+    </div>
+
+    <div class="developped">
+      <div>Developped for ConuHacks VIII</div>
+      <div class="team">
+        <div class="github">
+          <div><GithubLogo /></div>
+          <div>GitHub</div>
+        </div>
+        <div class="devpost">
+          <div><DevpostLogo /></div>
+          <div>Devpost</div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
